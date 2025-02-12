f6b425a01f4ad72786a92fcfeb6f29bc153494c6
Anna Steckel <annaextrasteckel@gmail.com>
2025-02-11 21:48:39 -0800

    embedded schema, controllers, middleware, nav partial, index routes done;

--- models/user.js
+++ models/user.js
@@ -1,14 +1,23 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+    // maybe this should be "name" instead of "food"?
+    food: {
+        type: String,
+        required: true
+    }
+});
+
 const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    pantry: [foodSchema]
 });
 
 const User = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -7,43 +7,55 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+// import middleware functions
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
+// import foodsController
+const foodsController = require('./controllers/food.js');
+
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-  })
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
 );
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
+    res.render('index.ejs', {
+        user: req.session.user,
+    });
 });
 
 app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
+    if (req.session.user) {
+        res.send(`Welcome to the party ${req.session.user.username}.`);
+    } else {
+        res.send('Sorry, no guests allowed.');
+    }
 });
 
+// For this application, users must be signed in to view any of the routes associated with their pantry. Therefore, isSignedIn should come above the foods controller, but not before auth.
+app.use(passUserToView);
 app.use('/auth', authController);
+app.use(isSignedIn);
+// use middleware to direct incoming requests to /users/:userId/foods to the foodsController
+app.use('/users/:userId/foods', foodsController);
 
 app.listen(port, () => {
-  console.log(`The express app is ready on port ${port}!`);
+    console.log(`The express app is ready on port ${port}!`);
 });
