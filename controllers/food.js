cb3199910974ef659b795bc3332a2a8a9d519b4b
Anna Steckel <annaextrasteckel@gmail.com>
2025-02-12 16:21:24 -0800

    create route created

--- controllers/food.js
+++ controllers/food.js
@@ -6,12 +6,36 @@ const User = require('../models/user.js');
 // ROUTER LOGIC HERE
 // '/users/:userId/foods' --> beginning of the link because of the middleware in server.js
 
+// INDEX
 router.get('/', (req, res) => {
     res.render('foods/index.ejs')
 });
 
+// NEW
 router.get('/new', async (req, res) => {
     res.render('foods/new.ejs')
 });
+
+// CREATE
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+// SHOW
+
+// EDIT
+
+// UPDATE
+
+// DELETE
+
 
 module.exports = router;
